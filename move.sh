fcd6a3ec853658a9664bc5526578d9ec51e0a893
JoniPonty <joniponty@protonmail.com>
2022-06-20 15:42:01 +0200

    fixed data remove bug

--- move.sh
+++ move.sh
@@ -1,5 +1,8 @@
-rm -r /var/www/html/data && rm -r /var/www/html/output
-mkdir /var/www/html/output && mkdir /var/www/html/data
-mv ./output/* /var/www/html/output && mv ./data/* /var/www/html/data
+rm -r /var/www/html/data
+rm -r /var/www/html/output
+mkdir /var/www/html/output
+mkdir /var/www/html/data
+mv ./output/* /var/www/html/output 
+mv ./data/* /var/www/html/data
 echo copy was succesful
 /etc/init.d/apache2 start
