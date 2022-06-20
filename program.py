7846ceb68ed9e40f4e84f7d9c3b5b402473ebf44
JoniPonty <joniponty@protonmail.com>
2022-06-20 20:37:57 +0200

    Added better Variable names

--- program.py
+++ program.py
@@ -4,46 +4,48 @@ import requests
 import os
 from functions import *
 
-link = str(sys.argv[1])
-r = requests.get(link)
-r = r.text
-r = re.split('href="|"', r)
-count = 0
-i = 0
-for x in r:
-    if re.match("^http[s]?:", x):
+arg_link = str(sys.argv[1])
+link_array = requests.get(arg_link)
+link_array = link_array.text
+link_array = re.split('href="|"', link_array)
+count = 1
+amount = 0
+for link in link_array:
+    if re.match("^http[s]?://.", link):
         count += 1
-a = open("./output/index.html", "w")
-b = open("./data/index.html", "w")
-a.write('<!DOCTYPE html><html lang="en"><head>    <meta charset="UTF-8">    <title>index</title></head><body>')
-b.write('<!DOCTYPE html><html lang="en"><head>    <meta charset="UTF-8">    <title>index</title></head><body>')
-r.append(sys.argv[1])
-for x in r:
-    if re.match("^http[s]?:", x):
-        i+=1
-        if "\\" in x:
-            x = x.replace("\\", "")
-        print(str(i) + " / " + str(count) + "    " + x)
+output_index = open("./output/index.html", "w")
+data_index = open("./data/index.html", "w")
+output_index.write('<!DOCTYPE html><html lang="en"><head>    <meta charset="UTF-8">    <title>index</title></head><body>')
+data_index.write('<!DOCTYPE html><html lang="en"><head>    <meta charset="UTF-8">    <title>index</title></head><body>')
+link_array.append(sys.argv[1])
+for link in link_array:
+    if re.match("^http[s]?://.", link):
+        amount+=1
+        if "\\" in link:
+            link = link.replace("\\", "")
+        print(str(amount) + " / " + str(count) + "    " + link)
         try:
-            ra = requests.get(x)
-            if ra.status_code == 200:
+            request = requests.get(link)
+            if request.status_code == 200:
                 try:
-                    name = getName(ra.text)
-                    f = open("./output/" + name, "w")
-                    a.write('<a href="' + name + '"><p>' + name + '</p></a>')
-                    a.write("\n")
-                    f.write(ra.text)
-                    f.close()
+                    name = getName(request.text)
+                    file_output = open("./output/" + name, "w")
+                    output_index.write('<a href="' + name + '"><p>' + name + '</p></a>')
+                    output_index.write("\n")
+                    file_output.write(request.text)
+                    file_output.close()
                 except:
-                    name = getFile(x)
-                    o = open("./data/" + name, "w")
-                    b.write('<a href="' + name + '"><p>' + name + '</p></a>')
-                    b.write("\n")
-                    o.write(ra.text)
-                    o.close()
+                    name = getFile(link)
+                    file_data = open("./data/" + name, "w")
+                    data_index.write('<a href="' + name + '"><p>' + name + '</p></a>')
+                    data_index.write("\n")
+                    file_data.write(request.text)
+                    file_data.close()
         except:
             print("THREW AN ERROR")
 
-a.write('</body></html>')
-a.close()
+output_index.write('</body></html>')
+data_index.write('</body></html>')
+output_index.close()
+data_index.close()
 os.system("sudo bash move.sh")
